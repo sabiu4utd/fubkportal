39b1f42bf2ed905bc43640c144321cfa7af1599a
sabiu Lawali Tsafe <mobiledev@pi141.com>
2025-10-26 20:10:01 +0100

    Base url changed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/fubkportals/';
+$config['base_url'] = 'http://localhost/fubkportal/';
 
 date_default_timezone_set('Africa/Lagos');
 
